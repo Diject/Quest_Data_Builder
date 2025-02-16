b3db9ee6a4f60ffd56691da750cf7cc9a3540f96
Diject <alexkozlovv@gmail.com>
2025-02-16 20:53:44 +0300

    Create LeveledListItem.cs

---
+++ TES3/Variables/LeveledListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest_Data_Builder.TES3.Variables
+{
+    internal class LeveledListItem
+    {
+        public string Id { get; set; }
+        public uint Level { get; set; }
+
+        public LeveledListItem(string Id, uint Level)
+        {
+            this.Id = Id;
+            this.Level = Level;
+        }
+    }
+}
