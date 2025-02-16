a70bf1529994540537eebad1c1600cc5f7567e11
Diject <alexkozlovv@gmail.com>
2025-02-16 20:54:04 +0300

    added missing fields to leveledItem record

--- TES3/Records/LeveledItem.cs
+++ TES3/Records/LeveledItem.cs
@@ -1,5 +1,6 @@
 ﻿using Quest_Data_Builder.Core;
 using Quest_Data_Builder.Logger;
+using Quest_Data_Builder.TES3.Variables;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,33 @@ namespace Quest_Data_Builder.TES3.Records
     {
         public readonly string Type = RecordType.LeveledItem;
         public readonly string Id = "";
+        public readonly uint Data;
+        public readonly byte ChanceNone;
+        public readonly uint? Count;
         public readonly HashSet<string> CarriedItems = new(StringComparer.OrdinalIgnoreCase);
 
+        /// <summary>
+        /// Required PC's level by item id
+        /// </summary>
+        public readonly List<LeveledListItem> Items = new();
+
         public bool IsDeleted { get; private set; } = false;
+
+        public bool CalculateForEach
+        {
+            get
+            {
+                return (this.Data & 0x1) != 0;
+            }
+        }
+
+        public bool CalculateForAllLevelsLessOrEqualPC
+        {
+            get
+            {
+                return (this.Data & 0x2) != 0;
+            }
+        }
 
 
         public LeveledItem(RecordData recordData) : base(recordData)
@@ -34,6 +59,8 @@ namespace Quest_Data_Builder.TES3.Records
 
             using (var reader = new BetterBinaryReader(new MemoryStream(this.RecordInfo.Data)))
             {
+                LeveledListItem? lastItem = null;
+
                 while (reader.Position < reader.Length)
                 {
                     string field = reader.ReadString(4);
@@ -49,10 +76,40 @@ namespace Quest_Data_Builder.TES3.Records
                                 CustomLogger.WriteLine(LogLevel.Info, $"ID {Id}");
                                 break;
                             }
+                        case "DATA":
+                            {
+                                Data = reader.ReadUInt32();
+                                break;
+                            }
+                        case "NNAM":
+                            {
+                                ChanceNone = reader.ReadByte();
+                                break;
+                            }
+                        case "INDX":
+                            {
+                                Count = reader.ReadUInt32();
+                                break;
+                            }
                         case "INAM":
                             {
                                 var itemId = reader.ReadNullTerminatedString(length);
                                 this.CarriedItems.Add(itemId);
+
+
+                                LeveledListItem item = new(itemId, 0);
+                                this.Items.Add(item);
+                                lastItem = item;
+                                break;
+                            }
+                        case "INTV":
+                            {
+                                uint pcLevel = reader.ReadUInt16();
+                                if (lastItem is not null)
+                                {
+                                    lastItem.Level = pcLevel;
+                                    lastItem = null;
+                                }
                                 break;
                             }
                         case "DELE":
