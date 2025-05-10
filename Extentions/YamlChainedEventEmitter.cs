dcc4154428b8373c15b8ed9bbc68365a6324b47f
Diject <alexkozlovv@gmail.com>
2025-05-10 20:35:34 +0300

    fix yaml formatting for double values

--- Extentions/YamlChainedEventEmitter.cs
+++ Extentions/YamlChainedEventEmitter.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.EventEmitters;
 
@@ -23,6 +25,18 @@ namespace Quest_Data_Builder.Extentions
                 {
                     Style = ScalarStyle.DoubleQuoted
                 };
+            }
+
+            if (eventInfo.Source.Type == typeof(double) || eventInfo.Source.Type == typeof(float))
+            {
+                var number = Convert.ToDouble(eventInfo.Source.Value);
+                var formatted = number % 1 == 0
+                    ? number.ToString("0.0", CultureInfo.InvariantCulture)
+                    : number.ToString("0.################", CultureInfo.InvariantCulture);
+
+                var scalar = new Scalar(null, null, formatted, ScalarStyle.Any, true, false);
+                emitter.Emit(scalar);
+                return;
             }
 
             base.Emit(eventInfo, emitter);
