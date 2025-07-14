e7fcde67877ec406354c60ac832de7b8fecc9fd7
Diject <alexkozlovv@gmail.com>
2025-07-14 21:45:47 +0300

    add InitializatorType enum

---
+++ Initializer/Types.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest_Data_Builder.Initializer
+{
+    enum InitializatorType
+    {
+        MorrowindHandler,
+        MO2Handler,
+        OMWHandler,
+    }
+}
