a06c23739d6facddc8958f89afe80a771ef9a8a8
Diject <alexkozlovv@gmail.com>
2025-07-14 21:42:52 +0300

    add FileLocator utility for resolving file paths

---
+++ Initializer/FileLocator.cs
@@ -0,0 +1,30 @@
+﻿using Quest_Data_Builder.Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest_Data_Builder.Initializer
+{
+    static class FileLocator
+    {
+        public static List<string> ResolveFullFilePaths(List<string> fileNames, List<string> directories)
+        {
+            List<string> res = new();
+            foreach (var fileName in fileNames)
+            {
+                foreach (var directory in directories)
+                {
+                    string filePath = Path.Combine(directory, fileName);
+                    if (File.Exists(filePath))
+                    {
+                        res.Add(filePath);
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
